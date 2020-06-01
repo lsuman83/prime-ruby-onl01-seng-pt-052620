caa9dda2464fb3b98fd1edb789ce1c1dc68f4177
Sean Bordelon <sean.j.bordelon@gmail.com>
2020-06-01 06:35:27 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,26 @@
-# Add  code here!
+def prime? (num)
+
+  array = []
+  
+  i = 2
+  
+  if num < 2
+    return false
+  end
+  
+  loop do
+    
+    array << i
+    break if i * i >= num
+    i += 1
+    
+  end
+  
+  array.each do |prime|
+    if num % prime == 0 && num != 2
+      return false
+    end
+  end
+  
+  return true
+end
