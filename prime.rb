bf05f67015452069f958388e59f93703ed8282e7
Sean Bordelon <sean.j.bordelon@gmail.com>
2020-06-01 06:02:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+
+def prime? (num)
+
+  array = []
+  
+  index = 0
+  
+  root = sqrt(num)
+  
+  (2..root.round).each do |numbers|
+      array << numbers
+  end
+  
+  prime_array = []
+  array.each do |prime|
+    if num % prime == 0
+      return false
+    end
+  end
+  return true
+end
